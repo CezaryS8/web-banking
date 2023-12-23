07153d2903f13bcd24eea7d2f883ce7f3ba61b49
cezarys8 <czarek.skorupski2001@gmail.com>
2023-12-23 17:24:15 +0100

    init

---
+++ backend/src/test/java/pl/cezary/webbanking/WebBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cezary.webbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
