84d95036a7ef64292584f149f84317ba5eef297f
cezarys8 <czarek.skorupski2001@gmail.com>
2023-12-23 18:53:20 +0100

    jwt auth with access and refresh tokens

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+# Run Spring Boot application
+```shell script
+mvn spring-boot:run
+```
+# Run following SQL script to create users roles and privileges
+```sql  
+INSERT INTO roles(name) VALUES('ROLE_USER');
+INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+```
+
+
---
+++ backend/src/main/java/pl/cezary/webbanking/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package pl.cezary.webbanking.repository;
+
+import pl.cezary.webbanking.models.ERole;
+import pl.cezary.webbanking.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
